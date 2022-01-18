e3ae5910889690c3dd30b08ac85f1a695f35c721
ZhuYing <liar1293215801@gmail.com>
2022-01-18 16:45:25 +0900

    fix import error owing to package missing

--- RRT/algorithm/__init__.py
+++ RRT/algorithm/__init__.py
@@ -1,3 +1,3 @@
 from RRT.algorithm.basicRRT import basicRRT
-from RRT_Star import RRT_Star
-from RRT_Connect import RRT_Connect
+from RRT.algorithm.RRT_Star import RRT_Star
+from RRT.algorithm.RRT_Connect import RRT_Connect
