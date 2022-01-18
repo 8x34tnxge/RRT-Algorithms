22ff13b1007214ea9941718a8e44bad936e96959
ZhuYing <liar1293215801@gmail.com>
2022-01-18 12:18:11 +0900

    add more RRT algs

--- RRT/tree.py
+++ RRT/tree.py
@@ -2,3 +2,12 @@ from algorithm.alglib.core import RRT_Template
 
 class basicRRT(RRT_Template):
     pass
+
+class RRTwithProb(RRT_Tempalte):
+    pass
+
+class RRT_Star(RRT_Tempalte):
+    pass
+
+class RRT_Connect(RRT_Tempalte):
+    pass
