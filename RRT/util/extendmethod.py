4ea98cfbbee97a36330fbea16c96c3d2bc3f3bca
ZhuYing <liar1293215801@gmail.com>
2022-03-03 22:11:44 +0900

    add docstring for directly_extend method

--- RRT/util/extendmethod.py
+++ RRT/util/extendmethod.py
@@ -3,5 +3,16 @@ from RRT.core import RRT as RRT
 
 
 def directly_extend(tree: RRT, new_node_info: np.ndarray, nearest_node_ID: int):
+    """the method to directly extend the tree from nearest node using new node
+
+    Parameters
+    ----------
+    tree : RRT
+        the tree storing the node and edge info
+    new_node_info : np.ndarray
+        the info of node to be inserted to tree
+    nearest_node_ID : int
+        the id of the nearest node from new node
+    """
     new_node_ID = tree.add_node(new_node_info)
     tree.add_edge(nearest_node_ID, new_node_ID)
